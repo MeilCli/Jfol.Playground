10f68d05e67db9d66b003d74d667ddd534b54328
meil <ics.sora.meil@gmail.com>
2019-10-15 00:26:34 +0900

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -23,13 +20,15 @@ describe('AppComponent', () => {
   it(`should have as title 'Playground'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Playground');
+    expect(app.title).toEqual("Playground");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Playground app is running!');
+    expect(compiled.querySelector(".content span").textContent).toContain(
+      "Playground app is running!"
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { format } from "jfol";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'Playground';
+  defaultJson = `{
+    "array": [
+        {
+            "package": {
+                "name": "pack1"
+            }
+        },
+        {
+            "package": {
+                "name": "pack2"
+            }
+        }
+    ]
+}`;
+  defaultJfol = `Packages Total: $$(array.length)
+$array[$$index: $(package.name)$$separator[\n]]`;
+
+  result = "";
+  resultTextColor = "#000";
+
+  json = this.defaultJson;
+  jfol = this.defaultJfol;
+
+  execute() {
+    try {
+      this.resultTextColor = "#000";
+      this.result = format(this.jfol, this.json);
+    } catch (error) {
+      this.resultTextColor = "#f00";
+      this.result = error.message;
+    }
+  }
 }
